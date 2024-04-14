0d479fa0824df45013c74e4af7dfa3a9e2af4df3
M Atheer <55885230+masreplay@users.noreply.github.com>
2024-04-14 08:27:08 +0300

    Add l10n_init command and flag

--- bin/l10n_init.dart
+++ bin/l10n_init.dart
@@ -1,7 +1,8 @@
 import 'dart:io';
 
+/// https://docs.flutter.dev/ui/accessibility-and-internationalization/internationalization#adding-your-own-localized-messages
 Future<void> l10nInit() async {
-  // run commands
+  /// add dependencies to pubspec.yaml
   await Process.run(
     'flutter',
     ['pub', 'add', 'flutter_localizations', '--sdk=flutter'],
@@ -10,4 +11,60 @@ Future<void> l10nInit() async {
     'flutter',
     ['pub', 'add', 'intl:any'],
   );
+
+  /// add flutter: generate: true to pubspec.yaml
+  addYamlKeys(
+    filename: "pubspec.yaml",
+    map: {
+      "flutter": {
+        "generate": true,
+      }
+    },
+  );
+}
+
+/// [map] = {
+///   "flutter": {
+///     "generate": true,
+///   }
+/// }
+///
+/// add on top level only here
+/// flutter:
+///   generate: true
+///
+/// don't add it here
+/// dependencies:
+///  flutter:
+///    sdk: flutter
+
+addYamlKeys({
+  required String filename,
+  required Map<String, dynamic> map,
+}) async {
+  final file = File(filename);
+  final lines = await file.readAsLines();
+  final newLines = <String>[];
+  var found = false;
+  for (var line in lines) {
+    if (line.contains('flutter:')) {
+      found = true;
+    }
+    if (found) {
+      if (line.contains('flutter:')) {
+        newLines.add(line);
+        for (var key in map.keys) {
+          newLines.add('  $key:');
+          for (var subKey in map[key].keys) {
+            newLines.add('    $subKey: ${map[key][subKey]}');
+          }
+        }
+      } else {
+        newLines.add(line);
+      }
+    } else {
+      newLines.add(line);
+    }
+  }
+  await file.writeAsString(newLines.join('\n'));
 }
